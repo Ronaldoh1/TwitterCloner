9950b40bf35679a8c2455802a37456a1b19e4c78
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-09-01 01:02:41 -0700

    able to sign up

--- Twitter/ViewController.swift
+++ Twitter/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+
+    var ref = Firebase (url: "https://twittercloner.firebaseio.com/")
+
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func signInButton(sender: AnyObject) {
 
+
+    }
+
+    @IBAction func signUp(sender: AnyObject) {
+
+
+        if emailText.text == "" || passwordText.text == ""{
+
+            println("Please make sure that you enter an email and a password")
+
+        }else {
+
+
+            ref.createUser(emailText.text, password: passwordText.text, withValueCompletionBlock: { (error, result) -> Void in
+
+                if error != nil {
+                    println(error)
+                }else{
+
+                    println("Success sign up")
+
+                }
+
+            })
+
+        }
+
+
+
+
+        
+    }
 }
 
